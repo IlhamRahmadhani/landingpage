027d7b80cebc3ebf1063e80f7a4ff2b39f375333
IlhamRahmadhani <ilham.rahmadhani@bakrie.ac.id>
2023-09-04 22:15:20 +0700

    set prefix db

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -59,6 +59,8 @@ abstract class BaseController extends Controller
         parent::initController($request, $response, $logger);
         // Preload any models, libraries, etc, here.
         $this->db = \Config\Database::connect();
+        $this->db->setPrefix('lp_');
+
         $this->setSettings();
         $this->mBanner = new Banner();
         $this->mBiaya = new Biaya();
